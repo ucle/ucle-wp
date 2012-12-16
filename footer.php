f3bf9c23e7afb611cfd5bc09c3e72c56e021d686
Jon Gjengset <jon@thesquareplanet.com>
2012-12-16 20:40:23 +0000

    Social footer

--- footer.php
+++ footer.php
@@ -9,6 +9,10 @@
   </div>
 
   <footer id="footer">
+    <?php $d = get_template_directory_uri(); ?>
+    <a class="icon facebook" href="https://www.facebook.com/uclentrepreneur"><img src="<?php echo $d; ?>/icons/facebook.png" alt="Facebook" /></a>
+    <a class="icon linkedin" href="http://www.linkedin.com/groups?gid=4699230"><img src="<?php echo $d; ?>/icons/linkedin.png" alt="LinkedIn" /></a>
+    <a class="icon twitter " href="https://twitter.com/uclentrepreneur"><img src="<?php echo $d; ?>/icons/twitter.png" alt="Twitter" /></a>
     <p>UCL Entrepreneurs Society &copy; 2012-<?php echo date('Y'); ?></p>
   </footer>
 </div>
