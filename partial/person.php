e1792fde3fe7a0084273baba610fcaebcc84c930
Jon Gjengset <jon@thesquareplanet.com>
2013-01-21 18:09:24 +0000

    Made person list responsive

--- partial/person.php
+++ partial/person.php
@@ -1,5 +1,7 @@
 <div class="person">
   <img src="<?php echo $img; ?>" alt="" />
   <h3 class="title"><?php echo $title; ?></h3>
-  <?php echo $blurb; ?>
+  <div class="blurb">
+    <?php echo $blurb; ?>
+  </div>
 </div>
